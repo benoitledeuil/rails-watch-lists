a18be5ab7c2f7a42fe9aeff1760682348c5d76a4
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-09-06 18:20:06 +0200

    list controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,35 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Placez ce code dans db/seeds.rb
+
+require 'open-uri'
+require 'json'
+
+puts 'Nettoyage de la base de données...'
+Movie.destroy_all
+
+puts 'Création des films...'
+
+# URL de l'API pour les films les mieux notés
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+# Lire et parser la réponse JSON
+response = URI.open(url).read
+data = JSON.parse(response)
+
+# URL de base pour les images
+base_image_url = 'https://image.tmdb.org/t/p/w500'
+
+# Créer un objet Movie pour chaque film
+data['results'].each do |movie_data|
+  movie = Movie.create!(
+    title: movie_data['title'],
+    overview: movie_data['overview'],
+    poster_url: base_image_url + movie_data['poster_path'],
+    rating: movie_data['vote_average']
+  )
+  puts "Créé #{movie.title}"
+end
+
+puts "Terminé ! #{Movie.count} films créés."
